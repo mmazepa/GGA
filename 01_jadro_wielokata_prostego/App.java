e7ebcac6f24bf499b7f68609ffda3b989c43a082
mmazepa <maksymilianmariusz@gmail.com>
2019-02-26 20:28:40 +0100

    Przygotowanie danych -> FileManager.

--- 01_jadro_wielokata_prostego/App.java
+++ 01_jadro_wielokata_prostego/App.java
@@ -1,6 +1,3 @@
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.util.ArrayList;
 
 public class App {
@@ -8,31 +5,11 @@ public class App {
     public static ArrayList<Point> polygon = new ArrayList<Point>();
     public static String inputFileName = "input.txt";
 
-    public static void stringToPoint(String stringToSplit) {
-        String[] splitted = stringToSplit.split(",");
-        Point p = new Point(Double.parseDouble(splitted[0]), Double.parseDouble(splitted[1]));
-        polygon.add(p);
-    }
+    public static void main(String args[]) {
+      FileManager fm = new FileManager();
+      polygon = fm.getData(inputFileName);
 
-    public static void preparePolygon() {
-        try {
-            FileInputStream fileInputStream = new FileInputStream(inputFileName);
-            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                stringToPoint(line);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String args[]) {    
-        preparePolygon();
-
-        for (int i = 0; i < polygon.size(); i++)
-            System.out.println("P(" + polygon.get(i).getX() + ", " + polygon.get(i).getY() + ")");
+      for (int i = 0; i < polygon.size(); i++)
+          System.out.println("P(" + polygon.get(i).getX() + ", " + polygon.get(i).getY() + ")");
     }
 }
---
+++ 01_jadro_wielokata_prostego/FileManager.java
@@ -0,0 +1,34 @@
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+public class FileManager {
+
+  public static Point fillTheList(String stringToSplit) {
+      String[] splitted = stringToSplit.split(",");
+      Point p = new Point(Double.parseDouble(splitted[0]), Double.parseDouble(splitted[1]));
+      return p;
+  }
+
+  public static ArrayList<Point> preparePolygon(String fileName) {
+      ArrayList<Point> polygon = new ArrayList<Point>();
+      try {
+          FileInputStream fileInputStream = new FileInputStream(fileName);
+          InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+          BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+          String line;
+          while ((line = bufferedReader.readLine()) != null) {
+              polygon.add(fillTheList(line));
+          }
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
+      return polygon;
+  }
+
+  public static ArrayList<Point> getData(String fileName) {
+    return preparePolygon(fileName);
+  }
+}
