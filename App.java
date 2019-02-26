f57b84485f6307796a0199af4e161fa1bba72724
Mariusz Mazepa <mmazepa@sigma.inf.ug.edu.pl>
2019-02-26 13:10:16 +0100

    Jądro wielokąta: Obsługa plików.

--- App.java
+++ App.java
@@ -1,6 +1,33 @@
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
 public class App {
+
+    public static ArrayList<Point> polygon = new ArrayList<Point>();
+
     public static void main(String args[]) {
-        System.out.println("Hello!");
-        Point x = new Point(1,2);
+        FileInputStream fileInputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+        
+        try {
+            fileInputStream = new FileInputStream("input.txt");
+            inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+            bufferedReader = new BufferedReader(inputStreamReader);
+            String s;
+
+            while ((s = bufferedReader.readLine()) != null) {
+                String[] var = s.split(",");
+                Point p = new Point(Double.parseDouble(var[0]), Double.parseDouble(var[1]));
+                polygon.add(p);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 3; i++)
+            System.out.println("P(" + polygon.get(i).getX() + ", " + polygon.get(i).getY() + ")");
     }
 }
